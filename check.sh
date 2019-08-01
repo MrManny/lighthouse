d0aec1d38f78ade8d94f57814d241f0ca6b6eba5
Manuel Warum <mwarum@gmail.com>
2019-08-01 10:52:26 +0200

    Using double brackets instead of single brackets
    As per static code analysis recommendation

--- check.sh
+++ check.sh
@@ -5,7 +5,7 @@ usage() {
   echo "This script begins a Lighthouse-based performance auditing of the given URL and stores them as JSON."
 }
 
-if [ $# -lt 1 ]
+if [[ $# -lt 1 ]]
 then
   echo "Missing URL as argument"
   usage
